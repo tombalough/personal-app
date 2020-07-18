84e9a90b43bc875650751dfb928eebf232d5f7f9
Tom Balough <tom@raven.black>
2020-07-17 18:24:43 -0700

    api hookup

--- app/components/books/Booklist.js
+++ app/components/books/Booklist.js
@@ -1,10 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+
+const gitHubUrl =
+  "https://cors-anywhere.herokuapp.com/" +
+  "https://www.goodreads.com/review/list/108291935.xml?key=0NOgToyDTBI3wOZX2gAg&v=2&shelf=read&sort =date_read";
 
 const Booklist = (props) => {
+  const [userData, setUserData] = useState({});
+
+  const apiCall = async () => {
+    const response = await Axios.get(gitHubUrl);
+    setUserData(response.data);
+  };
+
+  useEffect(() => {
+    apiCall();
+  }, []);
+  console.log(userData);
   return (
     <div>
       <h1>Test Test Test</h1>
+      <p>{userData.name}</p>
+      <p>{userData.public_repos}</p>
     </div>
   );
 };
